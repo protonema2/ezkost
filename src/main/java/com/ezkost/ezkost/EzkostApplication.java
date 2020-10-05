5b86a71d8103f3823f5bf17d5dacf405879f21e0
Felix Raphael <felixraphaelra@gmail.com>
2020-10-05 22:00:13 +0700

    First Commit

---
+++ src/main/java/com/ezkost/ezkost/EzkostApplication.java
@@ -0,0 +1,13 @@
+package com.ezkost.ezkost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzkostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzkostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezkost/ezkost/EzkostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezkost.ezkost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzkostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
